086835554ddb5df065664274f3ee0badebf0a922
dfl-zeke <zeke@defiland.app>
2021-09-27 02:33:09 +0400

    lil update for testing.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Attempt to reproduce dropped websocket notification
+# Solana RPC Websocket test
 
-create new keypair with `solana-new keygen -o test.json`
-Fund this keypair with: `solana airdrop 1 -k test.json -ut` or -ud
+Create new keypair with: `solana-keygen new -o test.json`
+Fund this keypair with: `solana airdrop 1 -k test.json -ut` or `solana airdrop 1 -k test.json -ud`
+Then run `yarn` to install dependencies
+Then run: `yarn start` to test 500 transaction
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import { Authorized, clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, StakeProgram } from '@solana/web3.js';
+import {
+    Authorized,
+    clusterApiUrl,
+    Connection,
+    Keypair,
+    PublicKey,
+    StakeProgram
+} from '@solana/web3.js';
 import fs from 'fs';
 
 const STAKE_PROGRAM_ID = new PublicKey('Stake11111111111111111111111111111111111111');
@@ -7,56 +14,57 @@ const PRIVATE_KEY = Buffer.from(JSON.parse(fs.readFileSync('test.json', 'utf8'))
 const KEYPAIR = Keypair.fromSecretKey(PRIVATE_KEY);
 
 async function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const RPC_POOL_ENDPOINT = ''
 const ENDPOINT = clusterApiUrl('devnet');
 const connection = new Connection(ENDPOINT, 'confirmed');
 
 async function main() {
-  const rentExemptRent = await connection.getMinimumBalanceForRentExemption(StakeProgram.space);
+    const rentExemptRent = await connection.getMinimumBalanceForRentExemption(StakeProgram.space);
 
-  connection.onProgramAccountChange(STAKE_PROGRAM_ID, ({accountId, accountInfo}) => {
-    console.log(`received ${accountInfo.lamports - rentExemptRent} ${accountId.toBase58()}`);
-  },
-  'confirmed',
-  [
-    {
-      memcmp: {
-        offset: 12,
-        bytes: KEYPAIR.publicKey.toBase58()
-      }
-    }
-  ]);
-  let index = 1;
-  while (1) {
-    const newKeypair = new Keypair();
-    const lamports = rentExemptRent + index;
-
-    const transaction = StakeProgram.createAccount({
-      fromPubkey: KEYPAIR.publicKey,
-      stakePubkey: newKeypair.publicKey,
-      authorized: new Authorized(
-        KEYPAIR.publicKey,
-        KEYPAIR.publicKey
-      ),
-      lamports
-    });
-    transaction.feePayer = KEYPAIR.publicKey;
-    //let recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-    //console.log(recentBlockhash);
-    //transaction.recentBlockhash = recentBlockhash;
-
-    const signature = await sendAndConfirmTransaction(
-      connection,
-      transaction,
-      [KEYPAIR, newKeypair],
+    connection.onProgramAccountChange(STAKE_PROGRAM_ID, (info) => {
+        const { accountId, accountInfo } = info
+        console.log(`received ${accountInfo.lamports - rentExemptRent} ${accountId.toBase58()}`);
+    },
+        'confirmed',
+        [
+            {
+                memcmp: {
+                    offset: 12,
+                    bytes: KEYPAIR.publicKey.toBase58()
+                }
+            }
+        ]
     );
-    console.log(`Sent ${index}`);
+    let index = 1;
+    while (index <= 500) {
+        const newKeypair = new Keypair();
+        const lamports = rentExemptRent + index;
 
-    await sleep(1000);
-    index++;
-  }
+        const transaction = StakeProgram.createAccount({
+            fromPubkey: KEYPAIR.publicKey,
+            stakePubkey: newKeypair.publicKey,
+            authorized: new Authorized(
+                KEYPAIR.publicKey,
+                KEYPAIR.publicKey
+            ),
+            lamports
+        });
+        transaction.feePayer = KEYPAIR.publicKey;
+        //let recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+        //console.log(recentBlockhash);
+        //transaction.recentBlockhash = recentBlockhash;
+
+        await connection.sendTransaction(
+            transaction,
+            [KEYPAIR, newKeypair],
+        );
+        console.log(`Sent ${index}`);
+
+        await sleep(2000);
+        index++;
+    }
 }
+
 main();
